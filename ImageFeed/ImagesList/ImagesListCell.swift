1109a56af04b55cf116ffeb1d40f70a0ee076586
nar11s <pavelakuz@gmail.com>
2025-12-31 02:43:48 +0300

    Выполнены задания по спринту 8

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Павел Кузнецов on 14.11.2025.
+//
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+}
